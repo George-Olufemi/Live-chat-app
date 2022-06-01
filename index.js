20d5d0948def54961abcf4281abe403cb0af86d4
George Olufemi <georgeoluolufemi@gmail.com>
2022-06-01 12:45:56 +0100

    done

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const express = require('express');
-//App setup
-const app = express();
+var express = require('express');
+var socket = require('socket.io');
 
-const server = app.listen(4000, function() {
-    console.log('listening for requests on port 4000');
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
 });
